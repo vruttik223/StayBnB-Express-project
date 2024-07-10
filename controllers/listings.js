d4c2224698230fd98b8d4b1793acbea6e4e6e46e
vruttik223 <vruttikghanekar@gmail.com>
2024-07-10 18:40:08 +0530

    done changes in flash msgs

--- controllers/listings.js
+++ controllers/listings.js
@@ -22,14 +22,14 @@ module.exports.renderNewForm=( req, res) => {
         .populate("owner"); // Ensure 'owner' field is populated correctly
   
       if (!listing) {
-        req.flash("error", "Listing not found");
+        req.flash("error", "Place not found");
         return res.redirect("/listings");
       }
   
       res.render("listings/show.ejs", { listing });
     } catch (err) {
-      console.error("Error fetching listing:", err);
-      req.flash("error", "Error fetching listing");
+      console.error("Error fetching place:", err);
+      req.flash("error", "Error fetching place");
       res.redirect("/listings");
     }
   };
@@ -57,14 +57,14 @@ module.exports.renderNewForm=( req, res) => {
     newListing.owner=req.user._id;
     newListing.image= {url,filename};
    await newListing.save();
-    req.flash("success","new listing created");
+    req.flash("success","new Place added");
     res.redirect("/listings");
   }
   module.exports.renderEditForm=async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
-      req.flash("error","listing you requested for doesnot exist");
+      req.flash("error","Place you requested for doesnot exist");
       res.redirect("/listings");
      }
     
@@ -79,7 +79,7 @@ if( typeof req.file !="undefined"){
     listing.image={url,filename};
     await listing.save();
 }
-    req.flash("success","listing updated");
+    req.flash("success","Place updated");
     res.redirect(`/listings/${id}`);
   }
 
@@ -87,6 +87,6 @@ if( typeof req.file !="undefined"){
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete({ _id: id });
     console.log(deletedListing);
-    req.flash("success"," listing deleted");
+    req.flash("success"," Place deleted");
     res.redirect(`/listings`);
   }
